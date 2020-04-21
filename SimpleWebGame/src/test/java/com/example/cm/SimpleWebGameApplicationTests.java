e26fcef1399a2d70ab7bb864b67bf9ace5321478
gkdals98@naver.com <chm0326@contela.com>
2020-04-21 16:31:54 +0900

    Init Backend

---
+++ SimpleWebGame/src/test/java/com/example/cm/SimpleWebGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
